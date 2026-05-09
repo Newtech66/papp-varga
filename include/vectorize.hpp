43e3d506ba6101f34efa108d5804f8cf08c46131
Newtech66 <contact.mainakroy@gmail.com>
2026-05-09 16:51:33 +0530

    Undo previous refactor

--- include/vectorize.hpp
+++ include/vectorize.hpp
@@ -6,13 +6,26 @@
 #include "common_typedefs.hpp"
 
 template<typename prec_type>
-optVector<prec_type> split(const Eigen::Ref<const optVector<std::complex<prec_type>>>& x);
+optVector<prec_type> split(const Eigen::Ref<const optVector<std::complex<prec_type>>>& x){
+    optVector<prec_type> v(2 * x.size());
+    v << x.real(), x.imag();
+    return v;
+}
 template<typename prec_type>
-optVector<std::complex<prec_type>> unsplit(const Eigen::Ref<const optVector<prec_type>>& x);
+optVector<std::complex<prec_type>> unsplit(const Eigen::Ref<const optVector<prec_type>>& x){
+    optVector<std::complex<prec_type>> v(x.size() / 2);
+    v.real() = x.head(x.size() / 2);
+    v.imag() = x.tail(x.size() / 2);
+    return v;
+}
 template<typename prec_type>
-optVector<prec_type> vec(const Eigen::Ref<const optMatrix<std::complex<prec_type>>>& X);
+optVector<prec_type> vec(const Eigen::Ref<const optMatrix<std::complex<prec_type>>>& X){
+    return split<prec_type>(X.template reshaped());
+}
 template<typename prec_type>
-optVector<prec_type> vec(const Eigen::Ref<const optMatrix<prec_type>>& X);
+optVector<prec_type> vec(const Eigen::Ref<const optMatrix<prec_type>>& X){
+    return X.template reshaped();
+}
 template<typename prec_type, bool is_complex>
 std::conditional_t<is_complex, optMatrix<std::complex<prec_type>>, optMatrix<prec_type>>
 unvec(const Eigen::Ref<const optVector<prec_type>>& x, int n);
